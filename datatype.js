df28e3a0371a4eeb25e5be3f3e0c911c64b327d7
sanakouser12 <145953353+sanakouser12@users.noreply.github.com>
2024-08-31 10:59:42 +0500

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,37 @@
+// datatype......
+// number
+let age=20;
+let hight=5;
+console.log(age,hight);
+//string
+let fullname ="sana";
+let depertment ="BSCS";
+console.log(fullname,depertment);
+//boolean 
+let pass ="true";
+console.log(pass);
+//undefined
+let  y ;
+console.log(y);
+//null
+let x = null;
+console.log(x);
+//########### conversion ###################
+ let num ="sana";
+ console.log (typeof(num))
+ let valueInNumber="number(num)";
+ console.log(valueInNumber);
+ //########## operator ######################
+ let value =11;
+ value++;
+ console.log(value);
+ //########### combination of  two strings############
+ let str1 ="sana";
+ let str2 ="kouser";
+ let str3 =str1+str2;
+ console.log( str3);
+ //############ combination of str and num ###########
+ console.log("sana" +1+2 );
+ 
+
+
