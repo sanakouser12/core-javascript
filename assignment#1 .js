ddf2d766a82af9205acdc51f174575a217864216
sanakouser12 <145953353+sanakouser12@users.noreply.github.com>
2024-09-01 21:37:49 +0500

    Add files via upload

---
+++ assignment#1 .js
@@ -0,0 +1,67 @@
+// **********Hello World Program**********
+console.log("Hello, World!");
+// - This line prints "Hello, World!" to the console.
+
+
+//**************varaible declare****************
+let name = "sana"; // string
+let age = 20; // number
+let pass = true; // boolean
+console.log(name,age,pass);
+//********change the values of varaible*****************
+let f_name  = "sana kouser";
+let  department ="bscs"
+let isAdmin= false;
+ console.log(f_name, department, isAdmin);
+
+ 
+ //Type Conversions*******************
+//*********************number******************* */
+let  num = "123";
+num= true;
+num=0;
+num=null;
+console.log(typeof (num));
+let valueInNumber = Number(num);
+console.log(valueInNumber);
+//***************boolen*********************************** */
+let  value = "123";
+value= true;
+value=0;
+value=null;
+console.log(typeof (value));
+let valueInBoolen = Boolean(value);
+console.log(valueInBoolen);
+//**********************STRING******************************
+let  score = "123"
+score= true;
+score=0;
+score=null;
+console.log(typeof (score));
+let valueInString = String(score);
+console.log(valueInString);
+
+//*************two number addsthem together and displays the result*****
+
+//let a= 5;
+//let b= 9;
+//sum=a+b;
+//let c=4;
+//let total=4*sum;
+//let result= total/ 2;
+//console.log(result);
+
+//compare two numbers and return a string
+console.log("sana" > "kouser")
+
+// ****************simple calculator function**********
+
+let d =3;
+let e=2;
+let sum= d+e;
+let subtract= d-e;
+let multiply= d*e;
+let divide= d/e;
+console.log(sum,subtract,multiply,divide)
+
+
